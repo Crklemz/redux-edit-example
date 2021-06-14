8c94f5a5368f997ab24b0e421c400c9d6fd601d7
Chris Klemz <crklemz@gmail.com>
2021-06-14 14:17:53 -0500

    notes complete

--- src/components/StudentDetail/StudentDetail.jsx
+++ src/components/StudentDetail/StudentDetail.jsx
@@ -1,15 +1,27 @@
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
+
 function StudentDetail(props) {
 
   const dispatch = useDispatch();
   const history = useHistory();
 
+  const handleClick = () => {
+    //dispatch to our edit reducer
+    dispatch({
+      type: 'SET_EDIT_STUDENT', 
+      payload: props.student
+    })
+
+    //navigate to the edit view
+    history.push('/edit')
+  }
+
   return (
     <tr>
       <td>{props.student.github_name}</td>
-      <td>Edit!</td>
+      <td onClick={handleClick}>Edit!</td>
     </tr>
   );
 }
--- src/index.js
+++ src/index.js
@@ -21,7 +21,24 @@ const studentList = (state = [], action) => {
 
 // hold only the single student object being edited
 const editStudent = (state  = {}, action) => {
-
+    
+    switch (action.type) {
+        case 'SET_EDIT_STUDENT':
+            //action.payload here is the DB row.object being edited
+            return action.payload;
+        case 'EDIT_ONCHANGE':
+        //update the github_name field, or whatever they sent, really
+        //keep whats not being changed the same
+        return {
+            ...state, 
+            //compute the value of action.payload.property
+            //make that the key
+            [action.payload.property]: action.payload.value
+        }
+        //change the sent property to the sent value
+        case 'EDIT_CLEAR':
+            return {}
+    }
     return state;
 }
 
